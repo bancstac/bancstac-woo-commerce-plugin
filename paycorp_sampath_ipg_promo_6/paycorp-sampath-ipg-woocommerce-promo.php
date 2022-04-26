d98fc1b3098bb13fede1b8111b8b0c896720c6f7
Elma <elma@bancstac.com>
2022-04-26 17:39:16 +0200

    Update for WooCommerce plugin version 6.3.1

--- paycorp_sampath_ipg_promo_6/paycorp-sampath-ipg-woocommerce-promo.php
+++ paycorp_sampath_ipg_promo_6/paycorp-sampath-ipg-woocommerce-promo.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) exit;
 Plugin Name: Paycorp Sampath Bank IPG PROMO 6
 License URI: https://www.paycorp.lk/
 Description: Sampath IPG by Paycorp 6 Month.
-Version: 1.8.6.3 - woocommerce 6.3
+Version: 1.8.6.3 - woocommerce 6.3.1
 Promo 6
 Author: Paycorp International
 
