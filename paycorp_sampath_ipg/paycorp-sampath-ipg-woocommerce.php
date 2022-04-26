ec32bf642750275c1ec88d734c0513bcbc414e3f
Elma <elma@bancstac.com>
2022-04-26 17:18:10 +0200

    Update for WooCommerce plugin version 6.1.1

--- paycorp_sampath_ipg/paycorp-sampath-ipg-woocommerce.php
+++ paycorp_sampath_ipg/paycorp-sampath-ipg-woocommerce.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) exit;
 Plugin Name: Paycorp Sampath Bank IPG
 License URI: https://www.paycorp.com.au/
 Description: Sampath IPG by Paycorp.
-Version: 1.8.6.1 - woocommerce 6.1.0
+Version: 1.8.6.1 - woocommerce 6.1.1
 Author: Paycorp International
 
 */
